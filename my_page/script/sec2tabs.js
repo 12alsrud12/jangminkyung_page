6bb0fae9af46b30c9ffe28538c5bf4bad2a7c3c4
12alsrud12 <12alsrud12@naver.com>
2024-09-08 00:25:47 +0900

    개인사이트

--- my_page/script/sec2tabs.js
+++ my_page/script/sec2tabs.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", () => {
     // 요소찾기
-    const btn1 = document.querySelectorAll("#sec2-btn>li")[0];
-    const btn2 = document.querySelectorAll("#sec2-btn>li")[1];
+    const btn1 = document.querySelectorAll("#sec2-btn>li a")[0];
+    const btn2 = document.querySelectorAll("#sec2-btn>li a")[1];
     const tab1 = document.querySelector("tab1");
     const tab2 = document.querySelector("tab2");
 
@@ -12,15 +12,15 @@ window.addEventListener("DOMContentLoaded", () => {
         tab2.classList.remove("on");
     };
 
-    btn1.onclick() = () => {
+    btn1.onclick(() => {
         resetT2();
         btn1.classList.add("on");
         tab1.classList.add("on");
-    };
+    });
 
-    btn2.onclick() = () => {
+    btn2.onclick(() => {
         resetT2();
         btn2.classList.add("on");
         tab2.classList.add("on");
-    };
+    });
 });
