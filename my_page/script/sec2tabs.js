412c2983cf0376679bc6b5267b0e2506fdc59145
12alsrud12 <12alsrud12@naver.com>
2024-09-07 18:11:06 +0900

    개인사이트

--- my_page/script/sec2tabs.js
+++ my_page/script/sec2tabs.js
@@ -1,3 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
-
+    const btn1 = document.querySelectorAll("#sec2-btn>li")[0];
+    const btn2 = document.querySelectorAll("#sec2-btn>li")[1];
+    const
 });
