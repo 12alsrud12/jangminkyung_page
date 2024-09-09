99128de4a261a85f864bd1811df68ae8d6199149
12alsrud12 <12alsrud12@naver.com>
2024-09-09 15:32:02 +0900

    개인사이트

--- my_page/script/script.js
+++ my_page/script/script.js
@@ -1,6 +1,18 @@
 window.addEventListener("DOMContentLoaded", () => {
-    // 요소찾기
-    const header = document.querySelector("header");
+    // 스크롤 이벤트를 위한 요소찾기
     const pppp = document.querySelector("#pppp");
+    // 헤더 이벤트 요소찾기
+    const header = document.querySelector("header");
 
+    // 스크롤이벤트
+    window.addEventListener("scroll", () => {
+        pppp.innerHTML = Math.floor(pageYOffset);
+
+        // header scroll event
+        if (pppp.innerHTML >= 900) {
+            header.classList.add("action");
+        } else {
+            header.classList.remove("action");
+        };
+    });
 });
