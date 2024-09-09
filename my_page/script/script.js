f0a76db4fbec753b643b3febf9cf534f613e40a2
12alsrud12 <12alsrud12@naver.com>
2024-09-09 14:34:19 +0900

    개인사이트

--- my_page/script/script.js
+++ my_page/script/script.js
@@ -1,40 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
 
-    const docEle = document.documentElement;
     const header = document.querySelector("header");
-    const sec = document.querySelectorAll('.fullscreen');
-    const wh = window.innerHeight;
 
-    for (let i = 0; i < sec.length; i++) {
-        sec[i].onwheel = (e) => {
-            // 기본 이벤트 방지
-            e.preventDefault();
-            if (e.deltaY > 0) {
-                // 마우스휠 내림
-                // e.currentTarget는 이벤트가 발생한 요소
-                if (docEle.scrollTop > wh * (sec.length - 2) + 100) return;
-                let next = e.currentTarget.nextElementSibling.offsetTop;
-                console.log('앗!', wh * (sec.length - 1) - 100, next);
-                docEle.scrollTop = next;
-                // 스크롤이벤트 (휠 내렸을 시)
-                if (next >= 500) {
-                    header.classList.add("action");
-                } else if (next = 0) {
-                    header.classList.remove("action");
-                }
-
-            } else if (e.deltaY < 0) {
-                // 마우스휠 올림
-                if (docEle.scrollTop < wh) return;
-                let prev = e.currentTarget.previousElementSibling.offsetTop;
-                console.log(prev);
-                docEle.scrollTop = prev;
-                // 스크롤이벤트 (휠 올렸을 시)
-                if (prev <= 0) {
-                    header.classList.remove("action");
-                }
-            }
-        };
-    };
 
 });
